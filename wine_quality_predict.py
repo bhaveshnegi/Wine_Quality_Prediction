3ab0e1ae5168ff5fba7175ee1f104c21fdf38158
bhaveshnegi <bhaveshnegi98@gmail.com>
2023-10-16 19:42:42 +0530

    remove warning

--- wine_quality_predict.py
+++ wine_quality_predict.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
+from warnings import filterwarnings
+filterwarnings(action='ignore')
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
